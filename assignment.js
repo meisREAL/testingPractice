85a9d2f71b627258243a9029d1dba761d974cf27
meisReal <simas.cesnakauskas@gmail.com>
2022-08-30 15:28:44 +0300

    functions to capitalize first letter, reverse a string and basic calculator object

--- assignment.js
+++ assignment.js
@@ -2,8 +2,33 @@ const capitalize = (string) => {
     const str = string.charAt(0).toUpperCase() + string.slice(1);
 
     return str;
+};
+
+const reverseString = (string) => {
+    let str = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        str += string[i];
+    };
+    return str;
+};
+
+const calculator = {
+    add: (a, b) => {
+        return a + b;
+    },
+    subtract: (a, b) => {
+        return a - b;
+    },
+    divide: (a, b) => {
+        return a / b;
+    },
+    multiply: (a, b) => {
+        return a * b;
+    }
 }
 
 export {
     capitalize,
+    reverseString,
+    calculator,
 }
--- assignment.test.js
+++ assignment.test.js
@@ -1,5 +1,22 @@
-import { capitalize } from './assignment'
+import { capitalize, reverseString, calculator } from './assignment'
 
 test('capitalize first character', () => {
     expect(capitalize('jurgis')).toBe('Jurgis');
+});
+
+test('reverse string', () => {
+    expect(reverseString('jurgis')).toBe('sigruj');
+});
+
+test('Calculator add', () => {
+    expect(calculator.add(2, 6)).toBe(8);
+});
+test('Calculator subtract', () => {
+    expect(calculator.subtract(10, 2)).toBe(8);
+});
+test('Calculator divide', () => {
+    expect(calculator.divide(12, 2)).toBe(6);
+});
+test('Calculator multiply', () => {
+    expect(calculator.multiply(3, 8)).toBe(24);
 });
